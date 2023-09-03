fb84ca126d039ba3f3df84fb5925cfc5b29650e4
Vlad Sanyuk <vlad.sanyuk@gmail.com>
2023-09-03 17:58:09 +0300

    fix tf version and do tf fmt

--- providers.tf
+++ providers.tf
@@ -1,9 +1,10 @@
 terraform {
+  required_version = ">= 1.5.6, < 2.0.0"
   backend "s3" {}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.65.0"
+      version = ">= 5.15.0, < 6.0.0"
     }
   }
 }
